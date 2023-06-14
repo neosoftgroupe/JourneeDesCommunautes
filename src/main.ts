80c20f2de68346e63bef271c97dfc7b43210e48c
neosoftgroupe <136612587+neosoftgroupe@users.noreply.github.com>
2023-06-14 19:12:40 +0200

    Add files via upload

--- src/main.ts
+++ src/main.ts
@@ -10,14 +10,86 @@ let currentPopup: any = undefined;
 WA.onInit().then(() => {
     console.log('Scripting API ready');
     console.log('Player tags: ',WA.player.tags)
-
-    WA.room.area.onEnter('clock').subscribe(() => {
-        const today = new Date();
-        const time = today.getHours() + ":" + today.getMinutes();
-        currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
+/*
+    WA.room.area.onEnter('indice1').subscribe(() => {
+        // const today = new Date();
+        // const time = today.getHours() + ":" + today.getMinutes();
+        currentPopup = WA.ui.openPopup("indice1Popup", "Je suis le Practice leader de la Practice DEVOPS, la lettre que tu recherches est la première de mon prénom !", []);
     })
 
-    WA.room.area.onLeave('clock').subscribe(closePopup)
+    WA.room.area.onLeave('indice1').subscribe(closePopup)
+
+    WA.room.area.onEnter('indice2').subscribe(() => {
+        currentPopup = WA.ui.openPopup("indice2Popup", "C'est l'indice 2", []);
+    })
+
+    WA.room.area.onLeave('indice2').subscribe(closePopup)
+
+    WA.room.area.onEnter('indice3').subscribe(() => {
+        currentPopup = WA.ui.openPopup("indice3Popup", "C'est l'indice 3", []);
+    })
+
+    WA.room.area.onLeave('indice3').subscribe(closePopup)
+
+    WA.room.area.onEnter('indice4').subscribe(() => {
+        currentPopup = WA.ui.openPopup("indice4Popup", "C'est l'indice 4", []);
+    })
+
+    WA.room.area.onLeave('indice4').subscribe(closePopup)
+
+    WA.room.area.onEnter('indice5').subscribe(() => {
+        currentPopup = WA.ui.openPopup("indice5Popup", "C'est l'indice 5", []);
+    })
+
+    WA.room.area.onLeave('indice5').subscribe(closePopup)
+
+    WA.room.area.onEnter('indice6').subscribe(() => {
+        currentPopup = WA.ui.openPopup("indice6Popup", "C'est l'indice 6", []);
+    })
+
+    WA.room.area.onLeave('indice6').subscribe(closePopup)
+
+    WA.room.area.onEnter('indice7').subscribe(() => {
+        currentPopup = WA.ui.openPopup("indice7Popup", "C'est l'indice 7", []);
+    })
+
+    WA.room.area.onLeave('indice7').subscribe(closePopup)
+
+    WA.room.area.onEnter('indice8').subscribe(() => {
+        currentPopup = WA.ui.openPopup("indice8Popup", "C'est l'indice 8", []);
+    })
+
+    WA.room.area.onLeave('indice8').subscribe(closePopup)
+
+    WA.room.area.onEnter('indice9').subscribe(() => {
+        currentPopup = WA.ui.openPopup("indice9Popup", "C'est l'indice 9", []);
+    })
+
+    WA.room.area.onLeave('indice9').subscribe(closePopup)
+*/
+
+    WA.room.area.onEnter('rs').subscribe(() => {
+        currentPopup = WA.ui.openPopup("rsPopup", "Viens nous suivre sur les réseaux sociaux !", [{
+            label: "Linkedin",
+            className: "primary",
+            callback: (popup) => {
+                WA.openTab('https://www.linkedin.com/company/neosoft_'),
+                // Close the popup when the "Close" button is pressed.
+                popup.close();
+            }    
+        },
+        {
+            label: 'Twitter',
+            className: 'primary',
+            callback: (popup) => {
+                WA.openTab('https://twitter.com/neosoft_'),
+                popup.close();
+            }
+        }]);
+
+    })
+
+    WA.room.area.onLeave('rs').subscribe(closePopup)
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
@@ -25,6 +97,7 @@ WA.onInit().then(() => {
     }).catch(e => console.error(e));
 
 }).catch(e => console.error(e));
+
 
 function closePopup(){
     if (currentPopup !== undefined) {
