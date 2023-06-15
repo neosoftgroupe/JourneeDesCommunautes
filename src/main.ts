38439bb4e2d070e7695a08bc89abb8aa39947b10
neosoftgroupe <136612587+neosoftgroupe@users.noreply.github.com>
2023-06-15 17:41:29 +0200

    Add files via upload

--- src/main.ts
+++ src/main.ts
@@ -67,7 +67,7 @@ WA.onInit().then(() => {
 
     WA.room.area.onLeave('indice9').subscribe(closePopup)
 */
-    WA.chat.sendChatMessage('Hello world', 'Mr Robot');
+    WA.chat.sendChatMessage('Hello world', WA.player.name);
 
     WA.room.area.onEnter('rs').subscribe(() => {
         currentPopup = WA.ui.openPopup("rsPopup", "Viens nous suivre sur les réseaux sociaux !", [{
