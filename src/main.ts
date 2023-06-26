7fc0b9af38b612979be383ad3c474214c7fba33b
neosoftgroupe <136612587+neosoftgroupe@users.noreply.github.com>
2023-06-26 10:39:03 +0200

    Add files via upload

--- src/main.ts
+++ src/main.ts
@@ -62,74 +62,17 @@ WA.onInit().then(() => {
     WA.room.area.onLeave('indice8').subscribe(closePopup)
 
     WA.room.area.onEnter('indice9').subscribe(() => {
-        currentPopup = WA.ui.openPopup("indice9Popup", "Mon tout est l'anagramme d'une plante connues pour ses jolies fleurs bleues !", []);
+        currentPopup = WA.ui.openPopup("indice9Popup", "Mon tout est l'anagramme d'une plante connues pour ses jolies fleurs bleues !", [{
+            label: "OK, merci !",
+            className: "primary",
+            callback: (popup) => {
+                popup.close();
+            }
+        }]);
     })
 
     WA.room.area.onLeave('indice9').subscribe(closePopup)
     
-
-    /*WA.room.area.onEnter('clock').subscribe(() => {
-        const today = new Date();
-        const time = today.getHours() + ":" + today.getMinutes();
-		if (time == 
-        currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
-    })*/
-
-    // Obtenir la date et l'heure actuelles
-    //var maintenant = new Date();
-
-    // Définir l'heure à laquelle tu souhaites afficher le calque
-    /*var heureAffichage = new Date();
-    heureAffichage.setHours(22); // Heure : 9 (exemple)
-    heureAffichage.setMinutes(40); // Minutes : 0 (exemple)
-    heureAffichage.setSeconds(0); // Secondes : 0 (exemple)
-    */
-    //WA.chat.sendChatMessage('Bienvenue à la Journée des Communautés ' + WA.player.name + ' !', " Mr Robot");
-    //WA.chat.sendChatMessage(maintenant.getTime(), " Mr Robot");
-    //WA.chat.sendChatMessage(heureAffichage.getTime(), " Mr Robot");
-    /*
-    function verifierHeure() {
-        // Obtenir l'heure actuelle
-        var maintenant = new Date();
-      
-        // Définir l'heure à laquelle tu souhaites déclencher l'action
-        var heureAction = new Date();
-        heureAction.setHours(23); // Heure : 9 (exemple)
-        heureAction.setMinutes(40); // Minutes : 0 (exemple)
-        heureAction.setSeconds(0); // Secondes : 0 (exemple)
-      
-        // Vérifier si l'heure actuelle correspond à l'heure d'action
-        if (maintenant.getTime() === heureAction.getTime()) {
-          // Déclencher ton action ici
-          //console.log("Action déclenchée à l'heure précise !");
-          WA.chat.sendChatMessage("Action déclenchée à l'heure précise !", " Mr Robot");
-        }
-      }
-      
-      // Vérifier l'heure toutes les secondes
-      setInterval(verifierHeure, 1000);
-    
-    WA.ui.banner.openBanner({
-        id: "banner",
-        text: "Ca va commencer en salle 1",
-        bgColor: "#051742",
-        textColor: "#FF5252",
-        closable: true,
-        //timeToClose: 12000,
-        link: {
-            label: "Test",
-            url: "https://workadventu.re"
-        }
-    });
-    
-    WA.ui.displayBubble()
-    
-    // Vérifier si l'heure actuelle est égale à l'heure d'affichage
-    if (maintenant.getTime() === heureAffichage.getTime()) {
-        // Afficher ton calque ou exécuter l'action souhaitée
-        WA.chat.sendChatMessage("C'est l'heure !", "Mr Robot");
-    }
-    */
 
     WA.room.area.onEnter('rs').subscribe(() => {
         currentPopup = WA.ui.openPopup("rsPopup", "Tu veux nous suivre sur les réseaux sociaux ?", [{
