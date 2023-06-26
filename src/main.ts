af8ef6f4426648e46f3b2512e9295d6c823a4977
neosoftgroupe <136612587+neosoftgroupe@users.noreply.github.com>
2023-06-26 18:16:46 +0200

    Add files via upload

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ WA.onInit().then(() => {
         // Définir l'heure à laquelle tu souhaites déclencher l'action
         var heureAction = new Date();
         heureAction.setHours(18); // Heure : 9 (exemple)
-        heureAction.setMinutes(16); // Minutes : 0 (exemple)
+        heureAction.setMinutes(22); // Minutes : 0 (exemple)
         heureAction.setSeconds(0); // Secondes : 0 (exemple)
       
         // Vérifier si l'heure actuelle correspond à l'heure d'action
@@ -31,7 +31,7 @@ WA.onInit().then(() => {
             text: "Banner test",
             bgColor: "#000000",
             textColor: "#ffffff",
-            timeToClose: 120000,
+            //timeToClose: 120000,
             link: {
                 label: "Test",
                 url: "https://workadventu.re"
